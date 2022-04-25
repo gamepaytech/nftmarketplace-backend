2e3c015dec88773d1b2ce3fc3f0cafbfe919c89e
ijaz-yenmin <ijaz.yenmin@gmail.com>
2022-04-25 13:20:00 +0530

    update collection name

--- controller/presaleController.js
+++ controller/presaleController.js
@@ -1,6 +1,6 @@
 
 const models = require("../models/presaleTier")
-const nftModels = require("../models/presaleNftTiers");
+const nftModels = require("../models/presaleNfts");
 const { mapReduce } = require("../models/Token");
 
 const startPresale = async (req, res) => {
