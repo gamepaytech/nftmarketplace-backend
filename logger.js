0ad5ad74df5b7090163aa0a424ad2c7a8066d8f3
Narasimhulu <narasimhulu1284@gmail.com>
2022-05-08 12:36:39 +0530

    Flag-based log enablement for S3
    Flag-based log enablement for S3

--- logger.js
+++ logger.js
@@ -1,48 +1,51 @@
 require('dotenv').config();
 const winston = require('winston');
-const { combine, timestamp,json } = winston.format;
+const { combine, timestamp, json } = winston.format;
 const S3StreamLogger = require('s3-streamlogger').S3StreamLogger;
+const os = require('os');
 
-const s3stream = new S3StreamLogger({
-    bucket:  process.env.S3_BUCKET_NAME,                
-    folder:  process.env.S3_FOLDER_NAME,        
-    tags: { project: 'nft-market-place-backend'},
-    access_key_id: process.env.S3_ACCESS_KEY,
-    secret_access_key: process.env.S3_SECRET_ACCESS_KEY
-});
+const transporters = [];
 
-const s3transport = new winston.transports.Stream({
+if(process.env.LOG_S3_ENABLED === 'true') {
+  const s3stream = new S3StreamLogger({
+    bucket: process.env.LOG_S3_BUCKET_NAME,
+    folder: process.env.LOG_S3_FOLDER_NAME,
+    tags: { project: 'nft-market-place-backend' },
+    access_key_id: process.env.LOG_S3_ACCESS_KEY,
+    secret_access_key: process.env.LOG_S3_SECRET_ACCESS_KEY
+  });
+  const s3transport = new winston.transports.Stream({
     stream: s3stream
-});
-
-s3transport.on('error', function(err){
-    logger.error("An error occured during s3 transport of logs");
-    logger.error(err);
-}); 
+  });
+  s3transport.on('error', function (err) {
+    console.log("An error occured during transfer of logs to S3");
+    console.log(err);
+  });
+  transporters.push(s3transport);
+} else {
+  transporters.push(new winston.transports.File({
+    filename: 'nftmarketplace-backend.log',
+  }));
+  transporters.push(new winston.transports.File({
+    filename: 'nftmarketplace-backend-error.log',
+    level: 'error',
+    format: combine(timestamp(), json()),
+  }));
+  transporters.push(new winston.transports.File({
+    filename: 'nftmarketplace-backend-info.log',
+    level: 'info',
+    format: combine(timestamp(), json()),
+  }));
+}
 
 const logger = winston.createLogger({
-    level: process.env.LOG_LEVEL || 'info',
-    format: combine(timestamp(), json()),
-    defaultMeta: {
-        hostName: 'nft-market-place-backend-code',
-        service: 'nft-backend-service',
-      },
-    transports: [
-        // new winston.transports.File({
-        //   filename: 'combined-2.log',
-        // }),
-        // new winston.transports.File({
-        //   filename: 'app-error-2.log',
-        //   level: 'error',
-        //   format: combine(timestamp(), json()),
-        // }),
-        // new winston.transports.File({
-        //   filename: 'app-info-2.log',
-        //   level: 'info',
-        //   format: combine(timestamp(), json()),
-        // }),
-        s3transport
-      ],
-  });
+  level: process.env.LOG_LEVEL || 'info',
+  format: combine(timestamp(), json()),
+  defaultMeta: {
+    hostName: os.hostname(),
+    service: 'nftmarketplace-backend',
+  },
+  transports: transporters
+});
 
-  module.exports = logger;
+module.exports = logger;
