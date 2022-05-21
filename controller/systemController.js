776b3c65530edcd2089cea6eb96a1cbf562c6810
Narasimhulu <narasimhulu@gmapay.sg>
2022-05-22 00:02:22 +0530

    Verify 304 error

--- controller/systemController.js
+++ controller/systemController.js
@@ -8,7 +8,7 @@ const logger = require('../logger')
 const getSysMessage = async (req, res) => {
     try {
         const msgCode = req.query.msgCode;
-        logger.info('Start of getting system message for code :: {}', msgCode);
+        logger.info('Start of getting system message for code :: '+ msgCode);
         const sysMsg = await getSystemMessage(msgCode);
         const message = sysMsg ? sysMsg.message : 'No system message found for this code';
         res.status(200).json({
@@ -25,7 +25,7 @@ const getSysMessage = async (req, res) => {
 const getSysMessageByLang = async (req, res) => {
     try {
         const { msgCode, lang } = req.query;
-        logger.info('Start of getting system message for code :: {} in {}', msgCode, lang);
+        logger.info('Start of getting system message for code :: '+ msgCode + ' in '+ lang);
         const sysMsg = await getSystemMessageByLang(msgCode, lang);
         const message = sysMsg ? sysMsg.message : 'No system message found for this code';
         res.status(200).json({
@@ -42,8 +42,12 @@ const getSysMessageByLang = async (req, res) => {
 const getSysConfig = async (req, res) => {
     try {
         const configName = req.query.configName;
-        logger.info('Start of getting system config for code :: {} ', configName);
+        console.log(configName);
+        logger.info(configName);
+        logger.info('Start of getting system config for code :: '+ configName);
         const sysConfig = await getSystemConfig(configName);
+        console.log(sysConfig);
+        logger.info(sysConfig);
         const configValue = sysConfig ? sysConfig.config_value : 'No system config found for this code';
         res.status(200).json({
             data: configValue
