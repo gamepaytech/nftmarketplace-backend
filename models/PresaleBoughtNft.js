1359a02a689beea217b04edf156484fb2ee19bb0
robinsaraswat0 <robinsaraswat9@gmail.com>
2022-07-01 12:28:17 +0530

    PriceTrail

--- models/PresaleBoughtNft.js
+++ models/PresaleBoughtNft.js
@@ -1,56 +1,57 @@
 const mongoose = require("mongoose");
 
 const PresaleBoughtNftSchema = new mongoose.Schema(
-    {
-        owner: {
-            type: String,
-            ref: "User",
-        },
-        nftIdOwned: {
-            type: mongoose.Schema.ObjectId,
-            ref:"User",
-        },
-        prevOwner:{
-            type:String,
-            ref:"User"
-        },
-        prevOwnerId:{
-            type:mongoose.Schema.ObjectId,
-            ref:"User"
-        },
-        nft: {
-            type: mongoose.Schema.ObjectId,
-            ref: "nftDetails",
-            required:true
-        },
-        amountSpent: {
-            type: Number,
-        },
-        promoCode: {
-            type: String,
-        },
-        quantity: {
-            type: Number,
-            default: 1
-        },
-        currency:{
-            type:String,
-            default:"USD"
-        },
-        paymentId: {
-            type:String
-        },
-        paymentMode: {
-            type:String
-        },
-        active:{
-            type:Boolean,
-            default:true
-        }
+  {
+    owner: {
+      type: String,
+      ref: "User",
     },
-    { timestamps: true,
-        strictPopulate:false }
-
+    nftIdOwned: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+    },
+    prevOwner: {
+      type: String,
+      ref: "User",
+    },
+    prevOwnerId: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+    },
+    nft: {
+      type: mongoose.Schema.ObjectId,
+      ref: "nftDetails",
+      required: true,
+    },
+    amountSpent: {
+      type: Number,
+    },
+    promoCode: {
+      type: String,
+    },
+    quantity: {
+      type: Number,
+      default: 1,
+    },
+    currency: {
+      type: String,
+      default: "USD",
+    },
+    paymentId: {
+      type: String,
+    },
+    paymentMode: {
+      type: String,
+    },
+    active: {
+      type: Boolean,
+      default: true,
+    },
+    saleId: {
+      type: Number,
+    },
+  },
+  { timestamps: true, strictPopulate: false }
 );
 
 module.exports = mongoose.model("PresaleBoughtNft", PresaleBoughtNftSchema);
