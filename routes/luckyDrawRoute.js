80f338a4d0e1eb093b15fd367a03b51e966b4edb
Gokul-Yenmin <gokul@yenmin.com>
2022-07-05 21:43:57 +0530

    Update luckyDrawRoute.js

--- routes/luckyDrawRoute.js
+++ routes/luckyDrawRoute.js
@@ -7,7 +7,6 @@ const {saveLuckyDrawTickets} = require("../controller/luckyDrawController");
 const {getMyEntries}= require("../controller/luckyDrawController");
 const {saveLuckyDrawConfig}=require('../controller/luckyDrawConfigController');
 const {getTotalEntries}=require('../controller/luckyDrawController');
-
 router.post('/',cors(), authenticateUser, saveLuckyDrawTickets);
 router.post('/user-entries',cors(), authenticateUser, getMyEntries);
 router.post('/total-entries',cors(), authenticateUser, getTotalEntries);
