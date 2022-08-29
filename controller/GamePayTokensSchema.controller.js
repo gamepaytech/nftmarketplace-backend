7c89eeca02aa5de12f1e35ff1644a5f16e79800b
Uddeshya Tiwari <uddeshyatiwari9085@gmail.com>
2022-08-29 22:45:05 +0530

    adding platform token route

--- controller/GamePayTokensSchema.controller.js
+++ controller/GamePayTokensSchema.controller.js
@@ -26,7 +26,16 @@ const getAllTokens = async (req, res) => {
     res.status(200).json(allTokens)
 }
 
+const getPlatformTokens = async (req, res) => {
+    const allTokens = await GamePayToken.find()
+    const platformTokens = allTokens.filter(token => {
+        if(token.name === 'Chiky' || token.name === 'Silk') return(token)
+    })
+    res.status(200).json(platformTokens)
+}
+
 module.exports = {
     getAllTokens,
     setTokens,
+    getPlatformTokens
 }
