f93fb7615e44ca3ec359ead05f5eb18081baa3f4
jagan <108288743+sasthawallet@users.noreply.github.com>
2022-09-06 10:41:11 +0530

    pagination api

--- routes/gamepay-listing/listingRoute.js
+++ routes/gamepay-listing/listingRoute.js
@@ -6,7 +6,7 @@ const { getGamepayListings, getTweetListByUsername, getRedditListByUsername, get
 const { uploadGamePayListing } = require('../../middleware/multerS3Upload');
 
 router.post('/',cors(),getGamepayListings);
-router.post('/filter-list',cors(),getGamepayListingByFilter);
+router.post('/filter-list/:page/:pageSize',cors(),getGamepayListingByFilter);
 router.get('/all-games',cors(),getGamepayListingAllGames);
 router.post('/get-tweet-list',cors(),getTweetListByUsername);
 router.post('/get-reddit-list',cors(),getRedditListByUsername);
