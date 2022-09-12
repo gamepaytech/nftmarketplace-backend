b5c587287db202b496c55f8cf177745071ad06b7
jagan <108288743+sasthawallet@users.noreply.github.com>
2022-09-12 17:23:05 +0530

    Update gameController.js

--- controller/gamepay-listing/gameController.js
+++ controller/gamepay-listing/gameController.js
@@ -1,5 +1,6 @@
 const logger = require('../../logger');
-const submitgame = require('../../models/gamepay-listing/game');
+// const submitgame = require('../../models/gamepay-listing/game');
+const submitgame = require('../../models/gamepay-listing/submitgame')
 const sendSubmitEmail = require('../../utils/sendSubmitEmail');
 
 const game = async(req,res)=>{
@@ -42,95 +43,149 @@ const game = async(req,res)=>{
         
          const check = emailRegexp.test(emailId)
          if (!check) {
-            return res.send(" Invalid email")
+            return res.status(400).json({
+               error : "Invalid email"
+              });
          }
          
          const url =  /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
          const image = /(http[s]?:\/\/.*\.(?:png|jpg|gif|svg|jpeg|webp))/i;
          const logovalidation =  image.test(logo);
          if (!logovalidation) {
-            return res.send("GameLogo image url is not valid ")
+            return res.status(400).json({
+               error : "GameLogo image url is not valid"
+              });
          }
          const mediavalidation =  image.test(media);
          if (!mediavalidation) {
-            return res.send("GameMedia image url is not valid ")
+            return res.status(400).json({
+               error : "GameMedia image url is not valid "
+              });
          }
          const thumbnailvalidation =  image.test(thumbnail);
          if (!thumbnailvalidation) {
-            return res.send("GameThumbnail  url is not valid ")
+            return res.status(400).json({
+               error : "GameThumbnail  url is not valid"
+              });
          }
          const websitevalidation =  url.test(website);
          if (!websitevalidation) {
-            return res.send("GameWebsite url is not valid ")
+            return res.status(400).json({
+               error : "GameWebsite url is not valid"
+              });
          }
          const trailervalidation =  url.test(trailer);
          if (!trailervalidation) {
-            return res.send("GameTrailer url is not valid ")
+            return res.status(400).json({
+               error : "GameTrailer url is not valid "
+              });
          }
          const coin =  url.test(coinMarketCapUrl);
          if (!coin) {
-            return res.send("CoinMarketCap url is not valid ")
+            return res.status(400).json({
+               error : "CoinMarketCap url is not valid"
+              });
          }
          const subreddit =  url.test(redditUrl);
          if (!subreddit) {
-            return res.send("Subreddit url is not valid ")
+            return res.status(400).json({
+               error : "Subreddit url is not valid"
+              });
          }   
          if (!gameStudioName) {
-            return res.send("GameStudio name is required ")
+            return res.status(400).json({
+               error : "GameStudio name is required"
+              });
          }
          if (!relatedGame) {
-            return res.send("RelatedGame name  is required ")
+            return res.status(400).json({
+               error : "RelatedGame name  is required"
+              });
          }
          if (!userName) {
-            return res.send("UserName is required ")
+            return res.status(400).json({
+               error : "UserName is required"
+              });
          }
          if (!address) {
-            return res.send("Organization Address is required ")
+            return res.status(400).json({
+               error : "Organization Address is required"
+              });
          }
          if (!designation) {
-            return res.send("Designation is required ")
+            return res.status(400).json({
+               error : "Designation is required"
+              });
          }
          if (!gameName) {
-            return res.send("GameName is required ")
+            return res.status(400).json({
+               error : "GameName is required"
+              });
          }
          if (!gameStatus) {
-            return res.send("StatusGame is required ")
+            return res.status(400).json({
+               error : "StatusGame is required"
+              });
          }
          if (!launchDate) {
-            return res.send("GameLaunch date is required ")
+            return res.status(400).json({
+               error : "GameLaunch date is required "
+              });
          }
          if (!description) {
-            return res.send("GameDescription name is required ")
+            return res.status(400).json({
+               error : "GameDescription name is required"
+              });
          }
          if (!price) {
-            return res.send("GamePrice is required ")
+            return res.status(400).json({
+               error : "GamePrice is required"
+              });
          }
          if (!tokenEarnings) {
-            return res.send("TokenEarnings is required ")
+            return res.status(400).json({
+               error : "TokenEarnings is required"
+              });
          }
          if (!genre) {
-            return res.send("GameGenre is required ")
+            return res.status(400).json({
+               error : "GameGenre is required "
+              });
          }
          if (!platforms) {
-            return res.send("Supported Platforms game is required ")
+            return res.status(400).json({
+               error : "Supported Platforms game is required "
+              });
          }
          if (!tokenContract) {
-            return res.send("TokenContracts is required ")
+            return res.status(400).json({
+               error : "TokenContracts is required "
+              });
          }
          if (!twitterUrl) {
-            return res.send("TwitterHandle is required ")
+            return res.status(400).json({
+               error : "TwitterHandle is required"
+              });
          }
          if (!partnersAuthorised) {
-            return res.send("Partners Authorised is required ")
+            return res.status(400).json({
+               error : "Partners Authorised is required"
+              });
          }
          if (!tnCOne) {
-            return res.send("Policy 1 is required ")
+            return res.status(400).json({
+               error : "Policy 1 is required"
+              });
          }
          if (!tnCTwo) {
-            return res.send("Policy 2 is required ")
+            return res.status(400).json({
+               error : "Policy 2 is required"
+              });
          }
          if (!tnCThree) {
-            return res.send("Policy 3 is required ")
+            return res.status(400).json({
+               error : "Policy 3 is required"
+              });
          }
         if (webVersion==="web 3.0") {
             const game = new submitgame({
@@ -221,6 +276,7 @@ const game = async(req,res)=>{
 
             }
             catch (error) {
+               console.log(error)
                logger.error(error);
             res.status(500).json({err:"Internal Server Error"})
              }
