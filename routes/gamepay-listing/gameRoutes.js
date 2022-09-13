db9628fe680a2c93cc973866c647d1c3fe5fc907
ijaz-yenmin <ijaz.yenmin@gmail.com>
2022-09-13 21:43:37 +0530

    auth validation with clientID & secret Key

--- routes/gamepay-listing/gameRoutes.js
+++ routes/gamepay-listing/gameRoutes.js
@@ -2,7 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 const { game } = require('../../controller/gamepay-listing/gameController');
+const { authenticateClientIdAndSecret } = require('../../middleware/authentication');
 
 router.post('/',game);
+router.post('/game-1', authenticateClientIdAndSecret,(req,res)=>{
+    return res.send("call ")
+});
+
 
 module.exports = router;
