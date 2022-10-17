1abd7b6d91d7ec06270d5acc1a885c525811f53a
Narasimhulu <narasimhulu@gmapay.sg>
2022-10-17 21:49:42 +0530

    Add overAll rating for a review

--- controller/gamepay-listing/reviewsController.js
+++ controller/gamepay-listing/reviewsController.js
@@ -30,6 +30,7 @@ const getGameReview = async (req, res) => {
           let usefulCount = 0;
           let notUsefulCount = 0;
           review.showOpinion = false;
+          review.overallRating = review.rating + review.funToPlay + review.abilityToEarn + review.affordability + review.easyToLearn;
           const hasAddedReview = review.userId === userId;
           if (!!userId) {
             review.showOpinion = !hasAddedReview;
