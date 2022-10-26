52a72a38731d1b567ab3f9732b2752f3417e3570
ijaz-yenmin <ijaz.yenmin@gmail.com>
2022-10-26 22:19:35 +0530

    update nft token module

--- routes/gamepay-listing/gameNftRoute.js
+++ routes/gamepay-listing/gameNftRoute.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const cors = require('cors');
-const { getGameNFTList, insertORUpdateGameNFTList } = require('../../controller/gamepay-listing/gameNFTController');
+const { getGameNFTList, insertORUpdateGameNFTDetails, updateGameNFTList, updateGameTokenDetails } = require('../../controller/gamepay-listing/gameNFTController');
 
 const router = express.Router();
 
+router.get('/list/update', cors(),updateGameNFTList); 
+router.get('/token/update', cors(),updateGameTokenDetails); 
+router.get('/insertOrUpdate', cors(),insertORUpdateGameNFTDetails); 
 router.get('/:page/:limit', cors(),getGameNFTList); 
-router.get('/insertOrUpdate', cors(),insertORUpdateGameNFTList); 
 
 
 module.exports = router;
