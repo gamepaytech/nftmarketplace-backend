33a090191ee2946622017807ba06311df409bede
Narasimhulu <narasimhulu@gmapay.sg>
2022-12-13 22:50:07 +0530

    Add cors and authenticate for quest api

--- routes/questsRoute.js
+++ routes/questsRoute.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 
 const { addQuest, getQuests, addUserQuest } = require('../controller/questsController');
+const { authenticateUser } = require('../middleware/authentication')
 
 const router = express.Router();
 router.post('/add', addQuest);
 router.post('/view', getQuests);
-router.post('/claim', addUserQuest);
+router.post('/claim', cors(),authenticateUser,addUserQuest);
 module.exports = router;
