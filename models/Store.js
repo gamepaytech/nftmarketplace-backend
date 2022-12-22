46a005561d2d25878a26fa334c97eb49d500aed1
jagan <108288743+sasthawallet@users.noreply.github.com>
2022-12-22 16:04:51 +0530

    store api

--- models/Store.js
+++ models/Store.js
@@ -2,14 +2,41 @@ const mongoose = require("mongoose");
 
 const Store = new mongoose.Schema(
     {
-    purchase :  {
+    storeCategory: {
       type: String,
-     
+      required: true,
+    }, 
+    storeLogo: {
+      type: String,
+      required: true,
+    }, 
+    // storeImage: {
+    //   type: String,
+    //   required: true,
+    // }, 
+    product: {
+      type: String,
+      required: true
     },
-    available :  {
-        type: String,
-       
-      },
+    description: {
+      type: String,
+      required: true
+    },
+    startDate: {
+      type:Date,
+      required: true
+    },
+    endDate: {
+      type:Date,
+      required: true
+    },
+    storePoints: {
+      type: Number,
+      required: true
+    },
+    // available: {
+    //     type: Number,
+    //   }, 
     },
   { timestamps: true }
 );
--- routes/StoreRoute.js
+++ routes/StoreRoute.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
-const { gpyStore } = require('../controller/StoreController');
+const { gpyStore,Purchase,getStore } = require('../controller/StoreController');
 
 const router = express.Router();
 router.post('/add',gpyStore);
+router.post('/point',Purchase);
+router.get('/view',getStore);
 
 
 
